a03028985892c95edfbcb2393e6f03e049696861
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-30 16:25:08 -0600

    working on axios call on WillBeReplacedByJulieFile.js

--- one-line-a-day-FE/src/components/WillBeReplacedByJulieFile.js
+++ one-line-a-day-FE/src/components/WillBeReplacedByJulieFile.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../auth/axiosWithAuth';
 
 export default function WillBeReplacedByJulieFile() {
     const [entry, setEntry] = useState()
-    
+
+    useEffect(() => {
+        const id = props.match.params.id
+
+        axiosWithAuth()
+            .get(`https://one-line-daily.herokuapp.com/api/entries/${id}`)
+            .then(response => {
+                setEntry(response.data.data)
+            })
+            .catch(error => console.log(error))
+    })
 }
