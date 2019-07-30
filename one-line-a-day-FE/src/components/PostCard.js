adbada929f21aa5b52afe025817445f74f39dbf7
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-30 15:33:06 -0600

    still working on PostCard.js

--- one-line-a-day-FE/src/components/PostCard.js
+++ one-line-a-day-FE/src/components/PostCard.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import { Card, Image, Grid, Responsive, Segment } from ‘semantic-ui-react’;
+
+const PostCardDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-bottom: 2px solid black;
+`;
+
+
+
+
+export default function PostCard({ date, title }) {
+    return(
+        <PostCardDiv>
+            <h3>{date}</h3>
+            <h2>{title}</h2>
+        </PostCardDiv>
+
+    )
+
+}
