86c606a2ac9d74c752ea7447c8382be6b1d97aa7
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-07-31 14:37:31 -0700

    placed sass files into styles folder (except variables.scss) and made each <PostCard />'s div clickable

--- one-line-a-day-FE/src/App.js
+++ one-line-a-day-FE/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import PrivateRoute from './auth/PrivateRoute';
-import './App.scss';
+import './styles/App.scss';
 
 // components
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import JournalEntries from './components/JournalEntries';
-import PostList from './components/Post/PostList';
 import SelectedPost from './components/Post/SelectedPost';
 import CreatePost from './components/JulieTestDataFolder/CreatePost';
 
@@ -17,8 +16,6 @@ function App() {
 			<Route exact path="/" render={props => <Login {...props} />} />
 			<Route exact path="/signup" render={props => <SignUp {...props} />} />
 			<PrivateRoute exact path="/home" component={JournalEntries} />
-			{/* <Route exact path="/home/:entryId" component={Selected} /> */}
-			{/* <Route exact path="/postlist" render={props => <PostList {...props} />} /> */}
 			<Route exact path="/post/:id" render={props => <SelectedPost {...props} />} />
 			<Route exact path="/createpost" component={CreatePost} />
 
