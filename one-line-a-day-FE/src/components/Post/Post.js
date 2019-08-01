dcc45e64768c482826fd0d27aefcd7db12895ba2
Julie Thrall <juliethrall10@gmail.com>
2019-08-01 09:54:42 -0700

    converted box shadow to semantic raised, deleted box shadow from post.scss

--- one-line-a-day-FE/src/components/Post/Post.js
+++ one-line-a-day-FE/src/components/Post/Post.js
@@ -2,43 +2,40 @@ import React from 'react';
 import './post.scss';
 import { Card, Grid, Container, Image } from 'semantic-ui-react';
 
-const Post = ({post}) => {
-		const {id, title, text, created_at } = post;
+const Post = ({ post }) => {
+	const { id, title, text, created_at } = post;
 
-		// formats the date
-    const changedate = created_at.split("-");
-    const item3 = changedate[2];
-    const item3split = item3.split("T");
-    const datedisplay =  `${changedate[1]}, ${item3split[0]}, ${changedate[0]}`;
+	// formats the date
+	const changedate = created_at.split('-');
+	const item3 = changedate[2];
+	const item3split = item3.split('T');
+	const datedisplay = `${changedate[1]}, ${item3split[0]}, ${changedate[0]}`;
 
+	return (
+		<Container text>
+			<div className="post-container">
+				<Grid container columns={1}>
+					<Grid.Column>
+						<Card raised fluid className="card-container">
+							<Card.Content>
+								<Image src={require('../../assets/quill-drawing-a-line.svg')} size="small" />
 
-    return (
-        <Container text>
-            <div className="post-container">
-                <Grid container columns={1}>
-									<Grid.Column>
-										<Card className="card-container">
-											<Card.Content>
-												<Image src={require('../../assets/quill-drawing-a-line.svg')} size="small" />
+								<Card.Header>
+									<span className="post-title">{title}</span>
+								</Card.Header>
 
-												<Card.Header>
-													<span className="post-title">{title}</span>
-												</Card.Header>
+								<Card.Meta>
+									<span className="date">{datedisplay}</span>
+								</Card.Meta>
 
-												<Card.Meta>
-														<span className='date'>{datedisplay}</span>
-												</Card.Meta>
-
-												<Card.Description>
-														{text}
-												</Card.Description>
-											</Card.Content>
-										</Card>    
-									</Grid.Column>
-                </Grid>
-            </div>
-        </Container>
-    )
+								<Card.Description>{text}</Card.Description>
+							</Card.Content>
+						</Card>
+					</Grid.Column>
+				</Grid>
+			</div>
+		</Container>
+	);
 };
 
 export default Post;
@@ -46,4 +43,3 @@ export default Post;
 //must credit on marketing page to use graphics
 // <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 //<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-
--- one-line-a-day-FE/src/components/PostCard.js
+++ one-line-a-day-FE/src/components/PostCard.js
@@ -2,25 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Grid } from 'semantic-ui-react';
 
-export default function PostCard({ title, date, id }) {
-		// formats the date
-		const changedate = date.split("-");
-		const item3 = changedate[2];
-		const item3split = item3.split("T");
-		const datedisplay =  `${changedate[1]}, ${item3split[0]}, ${changedate[0]}`;
-    
-    return(
-        <div className="post-card">
-					<Link to={`/post/${id}`}>
-						<Card raised fluid className="card-container" style={{padding: "50px 0", marginBottom: "25px"}}>
-								<Card.Content>
-										<Card.Header><div className="date">{datedisplay}</div></Card.Header>
-										<Card.Meta>
-												<div className="post-title">{title}</div>
-										</Card.Meta>
-								</Card.Content>
-						</Card>
-					</Link>
-        </div>
-    )
+export default function PostCard ({ title, date, id }) {
+	// formats the date
+	const changedate = date.split('-');
+	const item3 = changedate[2];
+	const item3split = item3.split('T');
+	const datedisplay = `${changedate[1]}, ${item3split[0]}, ${changedate[0]}`;
+
+	return (
+		<div className="post-card">
+			<Link to={`/post/${id}`}>
+				<Card raised fluid className="card-container" style={{ padding: '50px 0', marginBottom: '25px' }}>
+					<Card.Content>
+						<Card.Header>
+							<div className="date">{datedisplay}</div>
+						</Card.Header>
+						<Card.Meta>
+							<div className="post-title">{title}</div>
+						</Card.Meta>
+					</Card.Content>
+				</Card>
+			</Link>
+		</div>
+	);
 }
