13e2aacb5949e6ea20176488a94258b1f9431330
Julie Thrall <juliethrall10@gmail.com>
2019-08-01 09:48:20 -0700

    added semantic container to CreateEntry & EditEntry that will prevent the forms from being so wide on desktop

--- one-line-a-day-FE/src/components/editEntry/EditEntry.js
+++ one-line-a-day-FE/src/components/editEntry/EditEntry.js
@@ -1,48 +1,50 @@
 import React, { useState } from 'react';
-import { Form, TextArea } from 'semantic-ui-react';
+import { Form, TextArea, Container } from 'semantic-ui-react';
 import styled from 'styled-components';
-import '../../variables.scss'
+import '../../variables.scss';
 
 // components
 import EditPostNav from '../PostNav/EditPostNav';
 
-
 export const PostStyle = styled.div`
-margin-bottom: 20px;
-border: 1px solid lightgrey;
-padding: 20px;
+	margin-bottom: 20px;
+	border: 1px solid lightgrey;
+	padding: 20px;
 `;
 
+const EditEntry = (props) => {
+	const { id, text, title, user_id } = props.location.state.post.post;
 
-const EditEntry = props => {
-	const {id, text, title, user_id} = props.location.state.post.post;
-
-	const [entry, setEntry] = useState({
-		id: id,
-		title: title,
-		text: text,
-		user_id: user_id
+	const [
+		entry,
+		setEntry
+	] = useState({
+		id      : id,
+		title   : title,
+		text    : text,
+		user_id : user_id
 	});
 
-	const changeHandler = event => {
-		setEntry({...entry, [event.target.name]: event.target.value})
-	}
+	const changeHandler = (event) => {
+		setEntry({ ...entry, [event.target.name]: event.target.value });
+	};
 
 	return (
-	<div>
-		<EditPostNav entry={entry} routerProps={props} />
-
-		<PostStyle>
-			<Form>
-				<Form.Field>
-					<label>Edit Post</label>
-					<input name="title" value={entry.title} onChange={changeHandler} />
-				</Form.Field>
-				<TextArea name="text" value={entry.text} onChange={changeHandler} />
-			</Form>
-		</PostStyle>
-  </div>
-	)
-}
+		<div>
+			<EditPostNav entry={entry} routerProps={props} />
+			<Container text>
+				<PostStyle>
+					<Form>
+						<Form.Field>
+							<label>Edit Post</label>
+							<input name="title" value={entry.title} onChange={changeHandler} />
+						</Form.Field>
+						<TextArea name="text" value={entry.text} onChange={changeHandler} />
+					</Form>
+				</PostStyle>
+			</Container>
+		</div>
+	);
+};
 
 export default EditEntry;
