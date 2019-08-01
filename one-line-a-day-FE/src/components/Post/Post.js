e9f8c1523f7a6f790b5a6c09c977fe8f0efdae07
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-01 09:52:25 -0600

    updated logic On PostCard.js and Post.js to display the name of the month instead of the number of the month

--- one-line-a-day-FE/src/components/Post/Post.js
+++ one-line-a-day-FE/src/components/Post/Post.js
@@ -6,10 +6,42 @@ const Post = ({post}) => {
 		const {id, title, text, created_at } = post;
 
 		// formats the date
-    const changedate = created_at.split("-");
-    const item3 = changedate[2];
-    const item3split = item3.split("T");
-    const datedisplay =  `${changedate[1]}, ${item3split[0]}, ${changedate[0]}`;
+		const changedate = created_at.split("-");
+		const item3 = changedate[2];
+        const item3split = item3.split("T");
+        const month = changedate[1]
+        const day = item3split[0]
+        const year = changedate[0]
+        const dateword = () => {
+            if(month === "01"){
+                return "January"
+            } else if (month === "02") {
+                return "February"
+            } else if (month === "03") {
+                return "March"
+            } else if (month === "04") {
+                return "April"
+            } else if (month === "05") {
+                return "May"
+            } else if (month === "06") {
+                return "June"
+            } else if (month === "07") {
+                return "July"
+            } else if (month === "08") {
+                return "August"
+            } else if (month === "09") {
+                return "September"
+            } else if (month === "10") {
+                return "October"
+            } else if (month === "11") {
+                return "November"
+            } else if (month === "12") {
+                return "December"
+            } else {
+                return "Not a Month"
+            }
+        }
+		const datedisplay =  `${dateword()} ${day}, ${year}`;
 
 
     return (
