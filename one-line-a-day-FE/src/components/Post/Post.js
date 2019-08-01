6efbd97b93c2b099d4b38e4283da4e8a5f49e1c7
Julie Thrall <juliethrall10@gmail.com>
2019-07-31 19:10:06 -0700

    added image to container-card in post.js

--- one-line-a-day-FE/src/components/Post/Post.js
+++ one-line-a-day-FE/src/components/Post/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './post.scss';
-import { Card, Grid, Container } from 'semantic-ui-react';
+import { Card, Grid, Container, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const Post = ({post}) => {
@@ -26,6 +26,7 @@ const {id, title, text, created_at } = post;
                 <Grid.Column>
                 <Card className="card-container">
                 <Card.Content>
+                <Image src={require('../../assets/quill-drawing-a-line.svg')} size="small" />
                 <Card.Header><span className="post-title">{title}</span></Card.Header>
                 <Card.Meta>
                     <span className='date'>{datedisplay}</span>
@@ -45,4 +46,8 @@ const {id, title, text, created_at } = post;
 };
 
 export default Post;
+
+//must credit on marketing page to use graphics
+// <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
