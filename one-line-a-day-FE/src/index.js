321c545cf6ef50ff162203d44a51474755cf3f5e
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-01 12:25:44 -0700

    Added react-loader-spinner dependency. Refactored SelectedPost.js to show loading spinner while fetching post

--- one-line-a-day-FE/src/index.js
+++ one-line-a-day-FE/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import App from './App';
 
 ReactDOM.render(
